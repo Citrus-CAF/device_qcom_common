7b8a748172d5a8ad93a65dd0d0de01d098980fb0
Jake Weinstein <xboxlover360@gmail.com>
2015-10-11 18:46:41 +0000

    power: use power.$(TARGET_BOARD_PLATFORM)
    * power.qcom works on devices only when ro.hardware=qcom, which isn't true on all.
    
    Change-Id: I24a5fef153d4075f38bf1ff9508b7b10764fed74

--- base.mk
+++ base.mk
@@ -470,7 +470,7 @@ LIBQDUTILS := libqdutils
 LIBQDMETADATA := libqdMetaData
 
 #LIBPOWER
-LIBPOWER := power.qcom
+LIBPOWER := power.$(TARGET_BOARD_PLATFORM)
 
 #LLVM for RenderScript
 #use qcom LLVM
