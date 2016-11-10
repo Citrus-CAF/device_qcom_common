64a82f1d605964343b861ee896b0cb9b77b08748
Louis Popi <theh2o64@gmail.com>
2016-11-10 07:22:49 +0000

    Add Flag to use CM Ramdisk
    Change-Id: Ib8c352fd20782254196f5c3435f2aefa99c08fbd

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
 # files that live under device/qcom/common/rootdir/etc/
-
+ifneq ($(strip $(TARGET_USE_CM_RAMDISK)),true)
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qcom.early_boot.sh
 LOCAL_MODULE_TAGS  := optional eng
@@ -240,3 +240,4 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.recovery.qcom.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
+endif # TARGET_USE_CM_RAMDISK
