978e4760a1bf062ec2ac3e5296847ee752f18dc3
Adarsh-MR <adarshmr1998@gmail.com>
2017-01-03 16:28:22 +0000

    use Media codecs from device tree
    Signed-off-by: Adarsh-MR <adarshmr1998@gmail.com>

--- base.mk
+++ base.mk
@@ -810,8 +810,7 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
-    device/qcom/common/media/media_profiles.xml:system/etc/media_profiles.xml \
-    device/qcom/common/media/media_codecs.xml:system/etc/media_codecs.xml
+    device/qcom/common/media/media_profiles.xml:system/etc/media_profiles.xml
 
 ifeq ($(strip $(TARGET_USES_NQ_NFC)),true)
 PRODUCT_COPY_FILES += \
